8c61181659bf8f6125c53a8332f93ed380611b3b
vnnh <90565423+vnnh@users.noreply.github.com>
2021-09-20 18:45:57 -0500

    basic physics

--- src/components/molecules/CanvasView.tsx
+++ src/components/molecules/CanvasView.tsx
@@ -1,26 +1,57 @@
-import { useGLTF, Loader, TransformControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { useGLTF, Loader } from "@react-three/drei";
+import { Canvas, extend, useThree } from "@react-three/fiber";
 import { fs } from "@tauri-apps/api";
 import React, { Suspense, useEffect } from "react";
 import { byteToArrayBuffer } from "../../util/byteToArrayBuffer";
 import { useBinaryFBX } from "../../three/useBinaryFBX";
 import * as THREE from "three";
-import { Vector3 } from "three";
+import { MeshPhongMaterial, Vector3 } from "three";
 import { FlyControls } from "../../three/controls/FlyControls";
 import { usePrevious } from "../../util/usePrevious";
 import { Lightmap } from "../../three/Lightmap";
 import { useControls } from "leva";
+import { Debug, Physics, useCylinder, usePlane } from "@react-three/cannon";
+import { DragControls } from "three/examples/jsm/controls/DragControls";
 
+extend({ DragControls });
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace JSX {
+		interface IntrinsicElements {
+			dragControls: any;
+		}
+	}
+}
+
+const gridMaterial = new MeshPhongMaterial({ opacity: 0.2, transparent: true });
 const BaseScene: React.FunctionComponent = () => {
+	const [ref, api] = usePlane(() => ({
+		rotation: [-Math.PI / 2, 0, 0],
+	}));
 	return (
-		<mesh scale={1000} rotation={[-Math.PI / 2, 0, 0]} castShadow={true} receiveShadow={true} frustumCulled={false}>
-			<planeGeometry />
-			<meshPhongMaterial args={[{ color: "#c0c0c0" }]} />
-		</mesh>
+		<>
+			<mesh
+				ref={ref}
+				scale={1000}
+				rotation={[-Math.PI / 2, 0, 0]}
+				position={[0, 0, 0]}
+				castShadow={true}
+				receiveShadow={true}
+				frustumCulled={false}
+			>
+				<planeGeometry />
+				<meshPhongMaterial args={[{ color: "#c0c0c0" }]} />
+			</mesh>
+			<gridHelper args={[1000, 20, 0x000000, 0x000000]} material={gridMaterial} />
+		</>
 	);
 };
 
 const FBXScene: React.FunctionComponent<{ filePath: string }> = (props) => {
+	const camera = useThree((state) => state.camera);
+	const gl = useThree((state) => state.gl);
+
 	const fbx = useBinaryFBX({
 		bufferPromise: fs.readBinaryFile(props.filePath).then((value) => {
 			return byteToArrayBuffer(value);
@@ -47,11 +78,16 @@ const FBXScene: React.FunctionComponent<{ filePath: string }> = (props) => {
 	fbx.position.set(0, Math.max(size.y / 2, 1), 0);
 	fbx.rotation.set(0, 0, 0);
 
+	const [ref, api] = useCylinder(() => ({
+		mass: 1,
+		position: [0, 2, 0],
+	}));
 	return (
 		<>
-			<TransformControls key={fbx.name} enabled isTransformControls>
-				<primitive key={fbx.name} object={fbx} />
-			</TransformControls>
+			<group ref={ref}>
+				<primitive object={fbx} />
+			</group>
+			<dragControls enabled transformGroup args={[[fbx], camera, gl.domElement]} />
 		</>
 	);
 };
@@ -74,7 +110,7 @@ const Scene: React.FunctionComponent<{ filePath: string }> = (props) => {
 	if (props.filePath.endsWith(".fbx")) {
 		return (
 			<Lightmap {...lightmapProps}>
-				<FBXScene filePath={props.filePath} />
+				<FBXScene key={props.filePath} filePath={props.filePath} />
 				<BaseScene />
 			</Lightmap>
 		);
@@ -105,7 +141,11 @@ class CanvasView extends React.PureComponent<{ filePath: string }> {
 					<fog attach="fog" args={["#d0d0d0", 100, 600]} />
 					<FlyControls movementSpeed={5} rotateSpeed={Math.PI / 8} autoForward={false} dragToLook={true} />
 					<Suspense fallback={null}>
-						<Scene filePath={this.props.filePath} />
+						<Physics iterations={15} gravity={[0, -200, 0]} allowSleep={false}>
+							<Debug>
+								<Scene filePath={this.props.filePath} />
+							</Debug>
+						</Physics>
 					</Suspense>
 				</Canvas>
 				<Loader />
